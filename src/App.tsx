6fcf22f18178c49fbec26dc12f5b94e1623e6d9e
Igor <igorbezmestin@MacBook-Pro-Igor.local>
2024-02-20 16:15:09 +0300

    fix: added logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { GET_ALL_USERS, GET_ONE_USER } from "./query/user";
+import { useMutation, useQuery } from "@apollo/client";
+import { User } from "./types/user";
+import { CREATE_USER } from "./mutations/user";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, loading, error, refetch } = useQuery(
+    GET_ALL_USERS
+    //   {
+    //   pollInterval: 500, // pooling
+    // }
+  );
+  const { data: oneUser, loading: loadingOneUser } = useQuery(GET_ONE_USER, {
+    variables: {
+      id: 1,
+    }
+  });
+  const [newUser] = useMutation(CREATE_USER);
+
+  const [users, setUsers] = useState<User[]>([]);
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    if (!loading) {
+      setUsers(data.getAllUsers);
+    }
+  }, [data, loading]);
+
+  console.log(oneUser);
+
+  const addUser = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { data } = await newUser({
+        variables: {
+          input: {
+            username,
+            age,
+          },
+        },
+      });
+      console.log(data);
+      setUsername("");
+      setAge(0);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getAll = (e: React.MouseEvent) => {
+    e.preventDefault();
+    refetch();
+  };
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error</h1>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form className="form">
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(+e.target.value)}
+        />
+        <div className="btns">
+          <button onClick={addUser}>Создать</button>
+          <button onClick={getAll}>Получить</button>
+        </div>
+      </form>
+
+      <ul className="users">
+        {users.map((user) => (
+          <li key={user.id} className="user">
+            [{user.id}]: {user.username} - {user.age}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
